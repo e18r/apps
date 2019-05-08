b9c6b5c0d9c7ed13ef469aa47dea9a26f709a7f3
Stefanie Doll <stefie.doll@googlemail.com>
2019-05-08 10:42:52 +0200

    derive updates

--- packages/api-derive/src/staking/controllers.ts
+++ packages/api-derive/src/staking/controllers.ts
@@ -12,10 +12,8 @@ import { drr } from '../util/drr';
 function allBonds (api: ApiInterface$Rx, stashIds: Array<AccountId>) {
   return combineLatest(
     // FIXME Convert to multi
-    stashIds.map((id) =>{
-      console.log('ID', id.toString());
-      return (api.query.staking.bonded(id) as Observable<Option<AccountId>>)
-    }
+    stashIds.map((id) =>
+      (api.query.staking.bonded(id) as Observable<Option<AccountId>>)
     )
   );
 }
